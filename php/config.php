5ce69d1cbc718ed052a9937a7c00545bfa217ba1
Simon Haga <1498414+sewil@users.noreply.github.com>
2018-05-24 22:03:47 +0200

    hfghf

--- php/config.php
+++ php/config.php
@@ -1,9 +1,9 @@
 <?php
-define('DB_HOST', 'pallas.feralhosting.com');
-define('DB_USER', 'onepace');
-define('DB_PASSWORD', '7df8s7df8/#(');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'onepace_site');
+define('DB_PASSWORD', '12345');
 define('DB_NAME', 'onepace');
-define('DB_PORT', '31996');
+define('DB_PORT', '3306');
 define('DB_SOCKET', $_SERVER['DOCUMENT_ROOT'] . '/../../../private/mysql/socket');
 define('LOG_PATH', $_SERVER['DOCUMENT_ROOT'] . '/../../../phplogs');
 define('DB_CHARSET', 'utf8');
