2fac60f6478d3655e9c7728f6a33facce88e24ff
GuyBransgrove <guy.bransgrove@gmail.com>
2018-06-05 19:53:18 +1000

    Add SideBar Collapsing

---
+++ react/js/LocalStorageUtils.jsx
@@ -0,0 +1,5 @@
+export default class LocalStorageUtils {
+	static valueToBool = (value) => {
+		return value === "true"
+	}
+}
--- react/js/components/Side.jsx
+++ react/js/components/Side.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
+import { Glyphicon } from "react-bootstrap";
 
 export default class Side extends Component {
 	render() {
 		return (
 			<div className="side">
+				<h5 className="pull-right side-toggle" onClick={this.props.toggle}>
+					<Glyphicon glyph="triangle-left"/>
+				</h5>
 				<h2>Links</h2>
 				<ul>
 					<li><a target="_blank" rel="noopener noreferrer" href="http://forums.arlongpark.net/showthread.php?t=38681">Arlong Park Forums Thread</a></li>
