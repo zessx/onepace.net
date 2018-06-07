63154d9be07173bf50eceb4f1ebf34087f6c3b50
Simon Haga <1498414+sewil@users.noreply.github.com>
2018-06-08 00:52:39 +0200

    php cleanup

--- php/list_folders.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'config.php';
-$query = "https://api.openload.co/1/file/listfolder?login=" . API_LOGIN . "&key=" . API_KEY . (isset($_GET['folder_id']) ? "&folder={$_GET['folder_id']}" : "");
-
-$response = json_decode(file_get_contents($query), true);
-
-header('Content-Type: application/json; charset=utf-8');
-echo json_encode($response, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE|JSON_NUMERIC_CHECK);
-?>
