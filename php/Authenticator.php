8f63bd1d0c8ed618c16d5ad3d8f178ef85d10c71
Simon Haga <1498414+sewil@users.noreply.github.com>
2018-06-16 20:10:04 +0200

    Authenticator

---
+++ php/Authenticator.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'db_context.php';
+require_once 'config.php';
+class Authenticator {
+	static function authenticate($context, $token) {
+		$context->connect();
+		$stmt = $context->prepare("select * from users where token = ?;");
+		$stmt->bind_param("s", $token);
+		$rows = $context->get_result($stmt);
+		$context->disconnect();
+		return $rows > 0;
+	}
+}
+?>
